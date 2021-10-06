26a691e3c9b899583e6ba94b4fa21513076bd501
Marius van Wijk <mvan-wij@student.codam.nl>
2021-10-06 11:03:27 +0200

    added some basic structs/types

---
+++ include/structs.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   structs.h                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mvan-wij <mvan-wij@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/10/05 15:45:21 by mvan-wij      #+#    #+#                 */
+/*   Updated: 2021/10/06 11:02:05 by mvan-wij      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTS_H
+# define STRUCTS_H
+
+/**
+ * cmd: command/file to execute
+ * argv: { argv0, argv1, ... }
+ * pipe_to: next command in series, NULL if final
+ */
+typedef struct s_cmd_node
+{
+	char				*cmd;
+	char				**argv;
+	struct s_cmd_node	*pipe_to;
+}	t_cmd_node;
+
+/**
+ * file_in: file to put on stdin (NULL if no file specified)
+ * file_out: file to put on stdout (NULL if no file specified)
+ */
+typedef struct s_files
+{
+	char	*file_in;
+	char	*file_out;
+}	t_files;
+
+typedef enum e_status
+{
+	FAILURE,
+	SUCCESS
+}	t_status;
+
+#endif
