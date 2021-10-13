a6210082ddf06b1735c346bc3be4cb252a1089fa
Ruben Van duyneveldt <rvan-duy@f0r4s22.codam.nl>
2021-10-13 17:20:20 +0200

    echo

---
+++ src/builtins/echo.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   echo.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/10/07 15:30:01 by rvan-duy      #+#    #+#                 */
+/*   Updated: 2021/10/13 12:45:21 by rvan-duy      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "builtins.h"
+
+int	echo(t_cmd_node *nodes)
+{
+	int		i;
+	t_bool	n_flag;
+
+	i = 1;
+	n_flag = false;
+	while (contains_flag(nodes->argv[i], "-n") == true)
+	{
+		n_flag = true;
+		i++;
+	}
+	while (nodes->argv[i] != NULL)
+	{
+		ft_putstr_fd(nodes->argv[i], STDOUT_FILENO);
+		if (nodes->argv[i] != NULL)
+			ft_putchar_fd(' ', STDOUT_FILENO);
+		i++;
+	}
+	if (n_flag == false)
+		ft_putchar_fd('\n', STDOUT_FILENO);
+	return (EXIT_SUCCESS);
+}
