ecd0f30232b78a9e8987f1290bb804ca55742811
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2021-11-12 18:01:10 +0100

    updated export

--- src/builtins/builtin_export.c
+++ src/builtins/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/20 13:04:28 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2021/11/12 15:23:38 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2021/11/12 16:47:44 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,18 @@ static t_env_var	*dup_list(t_env_var *envp)
 	return (dupped_list);
 }
 
+// TODO: put in seperate file because prob need to use this more often
+static void	free_list(t_env_var *envp)
+{
+	while (envp)
+	{
+		free(envp->name);
+		free(envp->value);
+		free(envp);
+		envp = envp->next;
+	}
+}
+
 static t_status	list_export(t_env_var *envp)
 {
 	t_env_var	*sorted_envp;
@@ -69,22 +81,28 @@ static t_status	list_export(t_env_var *envp)
 		printf("declare -x %s=\"%s\"\n", sorted_envp->name, sorted_envp->value);
 		sorted_envp = sorted_envp->next;
 	}
+	free_list(sorted_envp);
 	return (SUCCESS);
 }
 
-// TODO: multiple export in 1 command
-// TODO: export list
-// TODO: miss casten naar unsigned char ofziets!
 // TODO: export if var already exists, replace it
 t_status	builtin_export(t_cmd_node *nodes, t_env_var *envp)
 {
 	t_env_var	*new_env_var;
+	size_t		i;
 
 	if (nodes->argv[1] == NULL)
 		return (list_export(envp));
-	new_env_var = env_new(nodes->argv[1]);
-	if (new_env_var == NULL)
-		return (FAILURE);
-	env_add(&envp, new_env_var);
+	i = 0;
+	dprintf(2, "Start..\n");
+	while (nodes->argv[i] != NULL)
+	{
+		// check for dups
+		new_env_var = env_new(nodes->argv[i]);
+		if (new_env_var == NULL)
+			return (FAILURE);
+		env_add(&envp, new_env_var);
+		i++;
+	}
 	return (SUCCESS);
 }
