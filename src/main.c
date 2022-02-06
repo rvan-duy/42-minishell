195cdd0d132b24ac0a6ef38b32e3366091fd45e3
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-02-06 16:19:50 +0100

    fixed all signals

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/05 16:51:05 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/02/03 16:22:43 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/02/06 16:18:32 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,40 +24,30 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-// https://stackoverflow.com/questions/68003253/error-implicit-declaration-of-function-rl-replace-line-is-invalid-in-c99-we
-// TODO: ctrl-C and ctrl-\ interrupt readline
-// TODO: ctrl-C displays a new prompt on a new line -- SIGINT
-// TODO: ctrl-\ does nothing ------------------------- SIGQUIT
-// TODO: rl_clear_history ---------------------------- deletes all the entries of the readline history
-// TODO: rl_on_new_line ------------------------------ tells readline that we moved onto a newline
-// TODO: rl_replace line
-// TODO: rl_redisplay -------------------------------- change what is displayed on the screen to the readline buffer
-
+// TODO: return value when program gets interrupted by signals!
 void	signal_handler(int sig)
 {
-	static bool is_first_time = false;
-
 	if (sig == SIGINT)
 	{
-		// write(STDOUT_FILENO, "\b\b  ", 5);
-		// ft_putstr_fd("\nminishell$> ", STDOUT_FILENO);
-		ft_putstr_fd("\nminishell$> ", STDOUT_FILENO);
+		ft_putstr_fd("minishell$> ", STDOUT_FILENO);
+		ft_putstr_fd(rl_line_buffer, STDOUT_FILENO);
+		ft_putstr_fd("  \n", STDOUT_FILENO);
+		rl_replace_line("", 0);
 		rl_on_new_line();
 		rl_redisplay();
-		rl_replace_line("", 0);
 	}
 	else if (sig == SIGQUIT)
 	{
-		if (is_first_time == true)
-			ft_putstr_fd("\b\b  ", STDOUT_FILENO);
-		is_first_time = true;
+		ft_putstr_fd("minishell$> ", STDOUT_FILENO);
+		ft_putstr_fd(rl_line_buffer, STDOUT_FILENO);
+		ft_putstr_fd("  \b\b", STDOUT_FILENO);
 	}
 	(void)sig;
 }
 
-static void init_signals(void)
+static void	init_signals(void)
 {
-	struct sigaction action;
+	struct sigaction	action;
 
 	action.sa_handler = signal_handler;
 	sigaction(SIGINT, &action, NULL);
@@ -78,9 +68,6 @@ int	main(int argc, char **argv, char **envp)
 		node = NULL;
 		if (line != NULL)
 		{
-			// DEBUG
-			// enter -> returns a line with \0
-			// dprintf(2, "line is not NULL [%s]\n", line);
 			if (line[0] != '\0')
 			{
 				parse_line(line, &node, envp);
@@ -91,7 +78,7 @@ int	main(int argc, char **argv, char **envp)
 		}
 		else
 		{
-			ft_putendl_fd("\b \b\b \bexit", STDOUT_FILENO);
+			ft_putendl_fd("exit", STDOUT_FILENO);
 			return (EXIT_SUCCESS);
 		}
 	}
