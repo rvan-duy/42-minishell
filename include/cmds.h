c9f0521d9552a9342af95928aa3c566b0b397ac0
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-02-09 20:37:59 +0100

    new stuff to header

--- include/cmds.h
+++ include/cmds.h
@@ -6,14 +6,17 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/08 16:50:30 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/02/08 19:32:13 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/02/09 20:30:56 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CMDS_H
 # define CMDS_H
 
-char	*cmd_get_absolute_path(const char *command);
-char	**cmd_create_execve_array(char *command, char **arguments);
+# include "structs.h"
+
+char		*cmd_get_absolute_path(const char *command);
+char		**cmd_create_execve_array(char *command, char **arguments);
+t_status	cmd_exec_file(t_cmd_node *nodes, t_env_var *envp);
 
 #endif
