4de2b8c0b939a97c51ad707e8bd7f2a47522a0db
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-02-11 12:24:37 +0100

    header file typo

--- include/safe.h
+++ include/safe.h
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/15 12:48:05 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/02/09 15:06:08 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/02/11 12:20:33 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ void		safe_close(int fd);
 int			safe_open(const char *filename, int oflag);
 void		safe_dup2(int fildes1, int fildes2);
 void		safe_chdir(const char *dir);
-void		safe_check_access(const char *cmd, int oflag);
+void		safe_check_access(char *absolute_path, const char *cmd, int oflag);
 
 #endif
