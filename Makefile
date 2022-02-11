1caf99fa1d4120fe05e2e234764363e01750c0d4
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-02-11 16:22:59 +0100

    refactor builtin_check

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: mvan-wij <mvan-wij@student.codam.nl>         +#+                      #
 #                                                    +#+                       #
 #    Created: 2021/10/06 11:47:27 by mvan-wij      #+#    #+#                  #
-#    Updated: 2022/02/11 13:37:15 by rvan-duy      ########   odam.nl          #
+#    Updated: 2022/02/11 16:22:43 by rvan-duy      ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -60,6 +60,7 @@ SOURCES	=	src/execute.c \
 			src/safe/safe_fork.c \
 			src/safe/safe_open.c \
 			src/utils_2.c \
+			src/builtins/builtin_check_and_exec.c \
 			src/builtins/builtin_echo.c \
 			src/builtins/builtin_cd.c \
 			src/builtins/builtin_pwd.c \
@@ -83,6 +84,7 @@ SOURCES	=	src/execute.c \
 			src/cmds/cmd_create_execve_array.c \
 			src/cmds/cmd_exec_file.c \
 			src/cmds/cmd_redirect_stdin.c \
+			src/cmds/cmd_redirect_stdout.c \
 			src/debug/debug_print_nodes.c \
 			src/ruben_tests/criterion/criterion_test_tmp.c
 
--- src/execute.c
+++ src/execute.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/06 11:36:39 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/02/09 20:27:15 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/02/11 16:21:41 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,98 +18,7 @@
 #include "cmds.h"
 #include <fcntl.h>
 #include <sys/wait.h>
-
-/**
- * Checks if the node is a builtin command, if so: executes it
- * @param nodes pointer to `t_cmd_node *`
- * @param envp pointer to `t_env_var *`
- * @return 0 if success - 1 in case of error - 2 if node is not a builtin command
- */
-static int	builtin_check_and_exec(t_cmd_node *nodes, t_env_var *envp)
-{
-	if (!ft_strncmp(nodes->cmd, "echo", ft_strlen(nodes->cmd) + 1))
-		return (builtin_echo(nodes));
-	if (!ft_strncmp(nodes->cmd, "pwd", ft_strlen(nodes->cmd) + 1))
-		return (builtin_pwd());
-	if (!ft_strncmp(nodes->cmd, "cd", ft_strlen(nodes->cmd) + 1))
-		return (builtin_cd(nodes, envp));
-	if (!ft_strncmp(nodes->cmd, "export", ft_strlen(nodes->cmd) + 1))
-		return (builtin_export(nodes, envp));
-	if (!ft_strncmp(nodes->cmd, "unset", ft_strlen(nodes->cmd) + 1))
-		return (buitlin_unset(nodes, envp));
-	if (!ft_strncmp(nodes->cmd, "env", ft_strlen(nodes->cmd) + 1))
-		return (builtin_env(envp));
-	if (!ft_strncmp(nodes->cmd, "exit", ft_strlen(nodes->cmd) + 1))
-		return (builtin_exit(nodes));
-	return (NO_BUILTIN);
-}
-
-/*
-
-	cmd: grep
-	argv: grep exit NULL
-	pipe_to: NULL
-	files:
-		enum: IN
-		file_name: Makefile
-		enum: OUT
-		file_name: grep_result
-
-*/
-
 #include <stdio.h>
-
-static void	redirect_stdout(t_files **files)
-{
-	int		fd;
-	size_t 	i;
-
-	i = 0;
-	if (files[i] == NULL)
-		dprintf(2, "files is NULL\n");
-	while (files[i])
-	{
-		if (files[i]->e_type == REDIRECT_OUTPUT)
-		{
-			fd = safe_open(files[i]->file_name, O_WRONLY | O_TRUNC | O_CREAT);
-			safe_dup2(fd, STDOUT_FILENO);
-			safe_close(fd);
-		}
-		else if (files[i]->e_type == APPENDING_REDIRECT_OUTPUT)
-		{
-			fd = safe_open(files[i]->file_name, O_WRONLY | O_APPEND | O_CREAT);
-			safe_dup2(fd, STDOUT_FILENO);
-			safe_close(fd);
-		}
-		i++;
-	}
-}
-
-static void	redirect_stdin(t_files **files)
-{
-	int		fd;
-	size_t	i;
-
-	i = 0;
-	if (files[i] == NULL)
-		dprintf(2, "files is NULL\n");
-	while (files[i])
-	{
-		if (files[i]->e_type == REDIRECT_INPUT)
-		{
-			fd = safe_open(files[i]->file_name, O_RDONLY);
-			safe_dup2(fd, STDIN_FILENO);
-			safe_close(fd);
-		}
-		else if (files[i]->e_type == HERE_DOCUMENT)
-		{
-			// TODO
-			dprintf(2, "HERE_DOC NOT IMPLEMENTED YET\n");
-			exit(EXIT_FAILURE);
-		}
-		i++;
-	}
-}
 
 // absolute path /home/bin/cat
 // relative path ../bin/cat
@@ -126,27 +35,23 @@ static void	execute_child_process(t_cmd_node *nodes, int write_fd, t_env_var *en
 		safe_close(write_fd);
 	}
 
-	redirect_stdin(nodes->files);
-	redirect_stdout(nodes->files);
+	cmd_redirect_stdin(nodes->files);
+	cmd_redirect_stdout(nodes->files);
 
 	ret = builtin_check_and_exec(nodes, envp);
-	if (ret == EXECUTE_SUCCESS)
+	if (ret == SUCCESS)
 		exit(EXIT_SUCCESS);
 	else if (ret == NO_BUILTIN)
 	{
 		absolute_path = cmd_get_absolute_path(nodes->cmd);
-		dprintf(STDERR_FILENO, "%s\n", nodes->argv[0]);
-		safe_check_access(absolute_path, X_OK);
-		dprintf(STDERR_FILENO, "%s\n", nodes->argv[0]);
+		safe_check_access(absolute_path, nodes->cmd, X_OK);
 		free(nodes->argv[0]);
 		nodes->argv[0] = absolute_path;
-		dprintf(STDERR_FILENO, "%s\n", nodes->argv[0]);
 		execve(nodes->argv[0], nodes->argv, env_list_to_arr(envp));
-		dprintf(STDERR_FILENO, "%s\n", nodes->argv[0]);
 		perror("execve");
 		exit(EXIT_FAILURE);
 	} 
-	else if (ret == EXECUTE_FAILURE)
+	else if (ret == FAILURE)
 	{
 		ft_putendl_fd("builtin_check_and_exec returned 1", STDERR_FILENO);
 		exit(EXIT_FAILURE);
@@ -189,35 +94,28 @@ static void handle_pipes(t_cmd_node *nodes, t_env_var *envp)
 	// wait
 }
 
+/**
+ * Reproduces the behavior of bash, the algorithm works slightly different based 
+ * on whether the command has pipes or not
+ * @param nodes pointer to `t_cmd_node *`
+ * @param envp pointer to `t_env_var *`
+ * @return 0 if bash command has succesfully been executed - 1 in case of an error
+ */
 t_status	execute_line(t_cmd_node *nodes, t_env_var *envp)
 {
+	t_status return_status;
+
 	if (nodes->pipe_to == NULL)
 	{
-		if (NO_BUILTIN == builtin_check_and_exec(nodes, envp))
-		{
-			// execute command + creating child
-			return (SUCCESS);
-		}
+		return_status = builtin_check_and_exec(nodes, envp);
+		if (return_status == NO_BUILTIN)
+			cmd_exec_file_with_child(nodes, envp);
+		else if (return_status == FAILURE)
+			return (FAILURE);
 	}
 	else
+	{
 		handle_pipes(nodes, envp);
+	}
 	return (SUCCESS);
 }
-
-// int	execute_line(t_cmd_node *nodes, t_env_var *envp)
-// {
-// 	const int is_builtin = builtin_check_and_exec(nodes, envp);
-	
-// 	if (is_builtin == NO_BUILTIN)
-// 	{
-// 		if (nodes->cmd != NULL)
-// 			handle_processes(nodes, envp);
-// 		return (SUCCESS);
-// 	}
-// 	else if (is_builtin == ERROR)
-// 	{
-// 		ft_putendl_fd("error: builtin_check_and_exec returned ERROR", STDERR_FILENO);
-// 		return (FAILURE);
-// 	}
-// 	return (SUCCESS);
-// }
