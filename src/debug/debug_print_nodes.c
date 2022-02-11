37efcbe457611f1df1705b8dc892f074ff7b83a6
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-02-11 13:45:41 +0100

    improved print message

--- src/debug/debug_print_nodes.c
+++ src/debug/debug_print_nodes.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/06 16:45:07 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/02/08 14:43:28 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/02/11 13:43:12 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static void	print_files(t_files **files)
+{
+	size_t	i;
+
+	i = 0;
+	printf("files:   ");
+	if (files[i] == NULL)
+	{
+		printf("NULL\n");
+		return ;
+	}
+	while (files[i])
+	{
+		printf("[%s - %u] ", files[i]->file_name, files[i]->e_type);
+		i++;
+	}
+	printf("\n");
+}
+
 /**
  * Prints out all the command nodes and its content
  * @param nodes pointer to `t_cmd_node *`
- * @return nothing
+ * @return Nothing
  */
 void	print_nodes(t_cmd_node *nodes)
 {
@@ -30,11 +49,12 @@ void	print_nodes(t_cmd_node *nodes)
 		i = 0;
 		printf("-------------- NODE [%zu] --------------\n", node_number);
 		printf("address: %p\n", nodes);
-		printf("cmd: %s\n", nodes->cmd);
-		printf("argv: ");
+		printf("cmd:     %s\n", nodes->cmd);
+		printf("argv:    ");
 		while (nodes->argv[i])
 			printf("[%s] ", nodes->argv[i++]);
 		printf("\n");
+		print_files(nodes->files);
 		printf("pipe_to: %p\n", nodes->pipe_to);
 		nodes = nodes->pipe_to;
 		node_number++;
