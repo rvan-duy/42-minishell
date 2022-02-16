2e7370e86a5aca40cfc849cd9f3d9385f527aaba
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-02-16 12:55:06 +0100

    final refactor to put everything together

--- src/execute.c
+++ src/execute.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/06 11:36:39 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/02/15 15:26:52 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/02/16 12:51:50 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 #include <stdio.h>
 
 /**
- * Reproduces the behavior of bash, the algorithm works slightly different based 
+ * Reproduces the behavior of bash, the algorithm works slightly different based
  * on whether the command has pipes or not
  * @param nodes pointer to `t_cmd_node *`
  * @param envp pointer to `t_env_var *`
@@ -29,13 +29,28 @@
  */
 t_status	execute_line(t_cmd_node *nodes, t_env_var *envp)
 {
+	int	ret;
+	int	pid;
+
 	if (nodes->pipe_to == NULL)
 	{
-		cmd_exec_single_file(nodes, envp, STDOUT_FILENO);
+		ret = builtin_check_and_exec(nodes, envp);
+		if (ret == SUCCESFULLY_EXECUTED_BUILTIN)
+			return (SUCCESS);
+		else if (ret == NO_BUILTIN)
+		{
+			pid = safe_fork();
+			if (pid == CHILD_PROCESS)
+				cmd_exec_single_file(nodes, envp, STDOUT_FILENO);
+			wait(&g_exit_status);
+			return (SUCCESS);
+		}
+		ft_putendl_fd("Error: builtin_check_and_exec == 1", STDERR_FILENO);
+		g_exit_status = FAILURE;
 	}
 	else
 	{
-		cmd_exec_multiple_files(nodes, envp); // change to new refactored function
+		cmd_exec_multiple_files(nodes, envp);
 	}
 	return (SUCCESS);
 }
