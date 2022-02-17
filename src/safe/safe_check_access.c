fd32059ea52f7fbbc3eb8833f5bc3dee91714c94
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-02-17 16:31:56 +0100

    removed unnecessary headers and err msg

--- src/safe/safe_check_access.c
+++ src/safe/safe_check_access.c
@@ -6,24 +6,19 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/15 16:36:25 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/02/11 12:46:19 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/02/17 16:31:26 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <sys/errno.h>
-#include <string.h>
 
 void	safe_check_access(char *absolute_path, const char *cmd, int oflag)
 {
 	if (access(absolute_path, oflag) < 0)
 	{
 		ft_putstr_fd(cmd, STDERR_FILENO);
-		ft_putstr_fd(": ", STDERR_FILENO);
-		ft_putendl_fd(strerror(errno), STDERR_FILENO);
+		ft_putendl_fd(": command not found", STDERR_FILENO);
 		free(absolute_path);
 		exit(EXIT_SUCCESS);
 	}
