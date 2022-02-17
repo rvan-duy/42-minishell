3cd32aa3c215dca503e3def874a1193369d4fa74
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-02-17 14:54:20 +0100

    error_handling_start

---
+++ src/error/error_is_dir.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   error_is_dir.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/02/17 14:40:49 by rvan-duy      #+#    #+#                 */
+/*   Updated: 2022/02/17 14:53:37 by rvan-duy      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <sys/stat.h>
+#include <unistd.h>
+
+/**
+ * Checks if path is a directory, is so exits the process with exit code 127
+ * @param path pointer to `char *`
+ * @return nothing
+ */
+void	error_is_dir(const char *path)
+{
+	struct stat	path_stat;
+
+	stat(path, &path_stat);
+	if (S_ISDIR(path_stat.st_mode))
+	{
+		ft_putstr_fd(path, STDERR_FILENO);
+		ft_putendl_fd(": is a directory", STDERR_FILENO);
+		exit(127);
+	}
+}
