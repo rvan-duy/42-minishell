4c01516940f8034b4fe09880c29ad12adac5e253
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-02-19 13:53:31 +0100

    refactor

--- src/safe/safe_dup2.c
+++ src/safe/safe_dup2.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/15 12:41:54 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/02/15 16:03:45 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/02/19 13:53:08 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * Safely dups fildes1 to fildes2
+ * @param fildes1 int containing a fd
+ * @param fildes2 int containing a fd
+ * @return nothing
+ */
 void	safe_dup2(int fildes1, int fildes2)
 {
 	if (fildes1 < 0 || fildes2 < 0 || fildes1 == fildes2)
