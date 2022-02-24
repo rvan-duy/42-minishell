e438239a93389f956db86461db65f05fac213075
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-02-24 15:32:12 +0100

    added missing include

--- libft/src/mem/ft_protect.c
+++ libft/src/mem/ft_protect.c
@@ -6,11 +6,12 @@
 /*   By: mvan-wij <mvan-wij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/16 13:56:20 by mvan-wij      #+#    #+#                 */
-/*   Updated: 2022/02/22 11:26:53 by mvan-wij      ########   odam.nl         */
+/*   Updated: 2022/02/23 17:09:17 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <unistd.h>
 #include "libft.h"
 
 void	*ft_protect(void *ptr)
