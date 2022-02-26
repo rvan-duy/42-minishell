047ad3bd557d8ac9d190afc73081b4603cb96c09
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-02-26 17:38:40 +0100

    changed prototype

--- include/builtins.h
+++ include/builtins.h
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/07 15:46:40 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/02/11 16:21:15 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/02/25 17:28:49 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 # define BUILTINS_H
 
 # include "structs.h"
+# include <stdbool.h>
 
 # define NO_BUILTIN 2
 
+bool		builtin_check(const char *cmd);
 int			builtin_check_and_exec(t_cmd_node *nodes, t_env_var *envp);
 t_status	builtin_pwd(void);
 t_status	builtin_env(t_env_var *envp);
