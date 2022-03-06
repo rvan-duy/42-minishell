20c91b930850063820ddff4d92b99c300b6b1afb
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-03-06 15:28:54 +0100

    fixed return status with multiple pipes

--- src/cmds/cmd_exec_multiple_files.c
+++ src/cmds/cmd_exec_multiple_files.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/09 14:59:10 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/02/24 20:14:46 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/03/06 15:28:27 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/errno.h>
-
-static size_t	node_len(t_cmd_node *nodes)
-{
-	size_t	len;
-
-	len = 0;
-	while (nodes)
-	{
-		len++;
-		nodes = nodes->pipe_to;
-	}
-	return (len);
-}
-
-static void	wait_for_all_processes(size_t node_amount)
-{
-	size_t	i;
-	int		exit_status;
-
-	i = 0;
-	while (i < node_amount)
-	{
-		wait(&exit_status);
-		g_exit_status = WEXITSTATUS(exit_status);
-		i++;
-	}
-}
 
 static void	exec_child(int read_pipe, int write_pipe,
 						t_cmd_node *nodes, t_env_var *envp)
@@ -61,11 +34,11 @@ static void	exec_child(int read_pipe, int write_pipe,
  */
 void	cmd_exec_multiple_files(t_cmd_node *nodes, t_env_var *envp)
 {
-	const size_t	node_amount = node_len(nodes);
 	int				previous_read_pipe;
 	pid_t			pid;
 	t_pipefds		pipe_fds;
 	size_t			command_index;
+	int				exit_status;
 
 	command_index = 0;
 	previous_read_pipe = STDIN_FILENO;
@@ -75,6 +48,8 @@ void	cmd_exec_multiple_files(t_cmd_node *nodes, t_env_var *envp)
 		pid = safe_fork();
 		if (pid == CHILD_PROCESS)
 			exec_child(previous_read_pipe, pipe_fds.write, nodes, envp);
+		wait(&exit_status);
+		g_exit_status = WEXITSTATUS(exit_status);
 		safe_close(pipe_fds.write);
 		previous_read_pipe = pipe_fds.read;
 		if (nodes->pipe_to == NULL)
@@ -82,5 +57,4 @@ void	cmd_exec_multiple_files(t_cmd_node *nodes, t_env_var *envp)
 		command_index++;
 		nodes = nodes->pipe_to;
 	}
-	wait_for_all_processes(node_amount);
 }
