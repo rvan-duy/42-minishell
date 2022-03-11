915a6c8d580adbe61fd8445053f3a0ad68ebc11a
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-03-11 15:09:58 +0100

    norm

--- include/safe.h
+++ include/safe.h
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/15 12:48:05 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/03/06 16:42:30 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/03/11 15:09:40 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # define CHILD_PROCESS 0
 
 pid_t		safe_fork(void);
-pid_t       safe_wait(int *arg);
+pid_t		safe_wait(int *arg);
 t_pipefds	safe_create_pipe(void);
 int			safe_open(const char *filename, int oflag);
 void		safe_close(int fd);
