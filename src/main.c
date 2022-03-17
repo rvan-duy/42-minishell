b9d7dbd7c67615c55a00b09d3a39ad4941a9fa4e
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-03-17 16:05:06 +0100

    marius

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/05 16:51:05 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/03/17 14:13:58 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/03/17 15:09:23 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,19 @@ static int	exit_shell(void)
 {
 	ft_putendl_fd("exit", STDOUT_FILENO);
 	return (EXIT_SUCCESS);
+}
+
+static void	free_nodes(t_cmd_node *node)
+{
+	t_cmd_node	*tmp;
+
+	while (node)
+	{
+		tmp = node;
+		node = node->pipe_to;
+		ft_free_arr((void ***)&tmp->argv);
+		free(tmp);
+	}
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -49,6 +62,8 @@ int	main(int argc, char **argv, char **envp)
 			add_history(line);
 			free(line);
 		}
+		free_nodes(node);
+		system("leaks minishell");
 	}
 	(void)argc;
 	(void)argv;
