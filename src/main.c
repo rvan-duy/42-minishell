14f5eba0ade16470f0d379227b09b4abc1c9136b
Mariusmivw <mvan-wij@student.codam.nl>
2022-03-21 18:26:15 +0100

    free empty lines

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/05 16:51:05 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/03/21 18:20:33 by mvan-wij      ########   odam.nl         */
+/*   Updated: 2022/03/21 18:24:51 by mvan-wij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,10 +55,12 @@ int	main(int argc, char **argv, char **envp)
 	t_env_var	*envp_linked;
 	t_cmd_node	*node;
 
+	line = NULL;
 	envp_linked = env_arr_to_list(envp);
 	while (1)
 	{
 		change_signals(signal_handler, SIG_IGN);
+		free(line);
 		line = readline("minishell$> ");
 		signal(SIGINT, SIG_IGN);
 		if (line == NULL)
@@ -69,7 +71,6 @@ int	main(int argc, char **argv, char **envp)
 		if (node != NULL)
 			execute_line(node, envp_linked);
 		add_history(line);
-		free(line);
 		free_nodes(node);
 	}
 	(void)argc;
