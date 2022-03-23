406f6cfc8ab0f9f47703e6b9a5e1db1ddec8ec64
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-03-23 10:41:25 +0100

    refactor

--- include/error.h
+++ include/error.h
@@ -6,14 +6,12 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/17 14:52:12 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/02/19 12:38:01 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/03/23 10:40:09 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERROR_H
 # define ERROR_H
-
-# define EXIT_CODE_IS_DIRECTORY 126
 
 void	error_is_dir(const char *path);
 
--- src/safe/safe_open.c
+++ src/safe/safe_open.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/15 12:41:06 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/03/23 10:31:48 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/03/23 10:37:58 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ int	safe_open(const char *filename, int oflag, bool in_child)
 	fd = open(filename, oflag, 0644);
 	if (fd < 0)
 	{
-		ft_putstr_fd("minishell: ", STDERR_FILENO);
+		ft_putstr_fd(PROGRAM_NAME, STDERR_FILENO);
+		ft_putstr_fd(": ", STDERR_FILENO);
 		ft_putstr_fd(filename, STDERR_FILENO);
 		ft_putstr_fd(": ", STDERR_FILENO);
 		perror("");
