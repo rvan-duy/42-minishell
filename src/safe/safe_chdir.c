e514d376954431234f303debb8a4eb17a6b836ba
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-03-23 11:49:12 +0100

    changed safe_chdir prototype

--- src/safe/safe_chdir.c
+++ src/safe/safe_chdir.c
@@ -6,19 +6,23 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/15 12:42:55 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/02/08 11:26:31 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/03/23 11:48:15 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "structs.h"
 #include <unistd.h>
 #include <stdio.h>
 
-void	safe_chdir(const char *dir)
+int	safe_chdir(const char *dir)
 {
 	if (chdir(dir) < 0)
 	{
-		ft_putstr_fd("cd: ", STDERR_FILENO);
+		ft_putstr_fd(PROGRAM_NAME, STDERR_FILENO);
+		ft_putstr_fd(": cd: ", STDERR_FILENO);
 		perror(dir);
+		return (FAILURE);
 	}
+	return (SUCCESS);
 }
